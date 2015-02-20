b2794909cdb81308d54b040abf4145891c3cf2f5
Kyle Hiner <kylehiner@gmail.com>
2015-02-20 15:48:25 -0500

    Can run and reproduce Halpha fit for one galaxy

--- FitHalpha.py
+++ FitHalpha.py
@@ -30,9 +30,9 @@ SIIb = 6733.
 T, F = True, False
 
 # Define the initial guesses
-guess = [2e-16, NIIa, 20., \
-    3e-16, NIIb, 20., \
-    3e-16, Ha, 20., \
+guess = [2e-16, NIIa, 10., \
+    3e-16, NIIb, 10., \
+    3e-16, Ha, 10., \
     1e-16, Ha, 50., \
     0.5e-16, SIIa, 10., \
     0.5e-16, SIIb, 10.]
@@ -46,12 +46,12 @@ tied = ['', '', 'p[17]', \
     '', '', '']
 
 # Define the limits on each parameter
-limits = [(0.1 * guess[0], 100.), (NIIa - 2., NIIa + 2.), (0., 100.), \
-    (0.1 * guess[3], 100.), (NIIb - 2., NIIb + 2), (0., 100.), \
-    (0.1 * guess[6], 100.), (Ha - 2., Ha + 2.), (0., 100.), \
+limits = [(0.1 * guess[0], 100.), (NIIa - 2., NIIa + 2.), (0.1, 100.), \
+    (0.1 * guess[3], 100.), (NIIb - 2., NIIb + 2), (0.1, 100.), \
+    (0.1 * guess[6], 100.), (Ha - 2., Ha + 2.), (0.1, 100.), \
     (0.1 * guess[9], 100.), (Ha - 2., Ha + 2.), (10., 100.),
-    (0.1 * guess[12], 100.), (SIIa - 2., SIIa + 2.), (0., 100.), \
-    (0.1 * guess[15], 100.), (SIIb - 2., SIIb + 2.), (0., 100.)]
+    (0.1 * guess[12], 100.), (SIIa - 2., SIIa + 2.), (0.1, 100.), \
+    (0.1 * guess[15], 100.), (SIIb - 2., SIIb + 2.), (0.1, 100.)]
 
 # State whether the limit should be enforced
 limited = [(T, F), (T, T), (F, T), \
@@ -93,7 +93,42 @@ while True:
     if good == 'y' or good == 'Y':
         break
     else:
-        new_ex_reg = input('\n Define regions to exclude, e.g. [4850., 4900., 6550., 6580.] ')
+        new_ex_reg = input('\n Define regions to exclude, e.g. [4850., 4900., 6550., 6580.] \n')
 
+
+# Perform the line fitting
+
+sp.plotter(xmin = xmin, xmax = xmax)
+
+while True:
+
+    try:
+        sp.specfit(fittype = 'gaussian', multifit = True, guesses = guess, \
+               tied = tied, limits = limits, limited = limited, fit_plotted_area = True)
+    except ValueError:
+        print '\n Fit crashed.'
+        again = input('\n Try reinitializing initial parameters and running again? (y/n) ')
+        if again == 'y' or again == 'Y':
+            guess = [2e-16, NIIa, 10., \
+                    3e-16, NIIb, 10., \
+                    3e-16, Ha, 10., \
+                    1e-16, Ha, 50., \
+                    0.5e-16, SIIa, 10., \
+                    0.5e-16, SIIb, 10.]
+            continue
+        else:
+            print '\n Quitting for now'
+            break        
+
+    good = input('\n Does this fit look good? (y/n) ')
+
+    if good == 'y' or good == 'Y':
+        break
+    else:
+        again = input('\n Try running again without changes? (y/n) ')
+        if again == 'y' or again == 'Y': continue
+        else:
+            print '\n Quitting for now'
+            break
 
         
